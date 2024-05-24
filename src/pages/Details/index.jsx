fb8c63509d2ee96a7153520ba7111e3b78b79ee0
lucasrpinto <lucasrop22@gmail.com>
2024-05-24 20:12:47 -0300

    V. 24.05.24 (Finalizado Details)

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,8 +1,12 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 
-import { Header } from '../../components/Header'
+import { Tag } from '../../components/Tag';
 import { Button } from '../../components/Button';
+import { Header } from '../../components/Header'
 import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
+
+
 
 
 export function Details() {
@@ -10,15 +14,35 @@ export function Details() {
   return(
     <Container>
       <Header />
+      
+      <main>
+        <Content>
 
-      <Section title="Links Úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-        </Links>
-      </Section>
+            <ButtonText title="Exluir Nota" />
 
-      <Button title="Voltar"/>
+            <h1>
+              Introdução ao React
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corporis delectus alias laboriosam, tenetur eos consequuntur, maxime rerum aspernatur quo quidem praesentium adipisci, voluptatem et ratione numquam. Nemo, praesentium nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate porro architecto iste, dignissimos ad obcaecati optio! Molestias aliquam natus amet, doloribus ea, dolor perferendis incidunt veritatis eos veniam repudiandae unde.
+            </p>
+
+            <Section title="Links Úteis">
+              <Links>
+                <li><a href="#">https://www.rocketseat.com.br/</a></li>
+                <li><a href="#">https://www.rocketseat.com.br/</a></li>
+              </Links>
+            </Section>
+
+            <Section title="Marcadores">
+              <Tag title="express" />
+              <Tag title="nodejs" />
+
+            </Section>
+
+            <Button title="Voltar"/>
+        </Content>
+      </main>
     </Container>
   )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -6,9 +6,15 @@ export const Container = styled.div`
     
     display: grid;
     grid-template-rows: 105px auto;
-    grid-template-areas: 
+    grid-template-areas:
     "header"
-    "content"
+    "content";
+
+    > main {
+        grid-area: content;
+        overflow-y: scroll;
+        padding: 64px 0;
+    }
 `;
 
 export const Links = styled.ul`
@@ -23,4 +29,30 @@ export const Links = styled.ul`
         }
     }
 `;
+
+export const Content = styled.div`
+    max-width: 550px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+
+    > button:first-child {
+        align-self: end;
+    }
+
+    > h1 {
+        font-size: 36px;
+        font-weight: 500;
+        padding-top: 64px;
+    }
+
+    > p {
+        font-size: 16px;
+        margin-top: 16px;
+        text-align: justify;
+    }
+`;
+
+
 
