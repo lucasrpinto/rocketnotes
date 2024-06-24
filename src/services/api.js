78297675b430200da5b578ab084771af3bdee582
lucasrpinto <lucasrop22@gmail.com>
2024-06-24 12:06:56 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     // URL da API Online (Não usar essa URL por nada seu animal)
-   // baseURL: "https://api-notes-t11r.onrender.com"
-     baseURL: "http://localhost:3333"
+    baseURL: "https://api-notes-t11r.onrender.com"
+     //baseURL: "http://localhost:3333"
 })
 
